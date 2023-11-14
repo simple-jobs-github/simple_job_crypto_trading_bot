b92ada2b4fedbe169fdd695e45f808aa188af9e5
ClimberMel <climbermel@gmail.com>
2023-11-13 19:26:09 -0800

    Update pricesGUI.py

--- code/pricesGUI.py
+++ code/pricesGUI.py
@@ -2,43 +2,48 @@ import tkinter as tk
 from tkinter import ttk
 import requests
 
-def get_crypto_prices(crypto_id, vs_currency='usd'):
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies={vs_currency}"
+class CryptoPriceCheckerApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Crypto Price Checker")
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad responses (4xx and 5xx)
-        data = response.json()
-        price = data[crypto_id][vs_currency]
-        return price
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching cryptocurrency price: {e}")
-        return None
+        # Create and configure widgets
+        self.crypto_label = tk.Label(master, text="Select Cryptocurrency:")
+        self.crypto_combobox = ttk.Combobox(master, values=['Bitcoin', 'Ethereum', 'Litecoin', 'BNB', 'Tether', 'XRP', 'USDC',
+                                                            'Solana', 'Lido Staked Ether', 'Cardano', 'Dogecoin', 'TRON', 'Polygon',
+                                                            'Chainlink', 'Dai', 'Monero', 'NEO', ' Celestia'])  # Add more cryptocurrencies as needed
+        self.fetch_button = tk.Button(master, text="Fetch Price", command=self.fetch_price)
+        self.result_label = tk.Label(master, text="")
 
-def fetch_price():
-    selected_crypto = crypto_combobox.get()
-    if selected_crypto:
-        price = get_crypto_prices(selected_crypto.lower())
-        if price is not None:
-            result_label.config(text=f"Current {selected_crypto.capitalize()} price: ${price}")
-        else:
-            result_label.config(text="Failed to fetch cryptocurrency price.")
+        # Place widgets in the window
+        self.crypto_label.grid(row=0, column=0, padx=10, pady=10)
+        self.crypto_combobox.grid(row=0, column=1, padx=10, pady=10)
+        self.fetch_button.grid(row=1, column=0, columnspan=2, pady=10)
+        self.result_label.grid(row=2, column=0, columnspan=2, pady=10)
 
-# Create main window
-root = tk.Tk()
-root.title("Crypto Price Checker")
+    def get_crypto_prices(self, crypto_id, vs_currency='usd'):
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies={vs_currency}"
 
-# Create and configure widgets
-crypto_label = tk.Label(root, text="Select Cryptocurrency:")
-crypto_combobox = ttk.Combobox(root, values=['Bitcoin', 'Ethereum', 'Litecoin'])  # Add more cryptocurrencies as needed
-fetch_button = tk.Button(root, text="Fetch Price", command=fetch_price)
-result_label = tk.Label(root, text="")
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for bad responses (4xx and 5xx)
+            data = response.json()
+            price = data[crypto_id][vs_currency]
+            return price
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching cryptocurrency price: {e}")
+            return None
 
-# Place widgets in the window
-crypto_label.grid(row=0, column=0, padx=10, pady=10)
-crypto_combobox.grid(row=0, column=1, padx=10, pady=10)
-fetch_button.grid(row=1, column=0, columnspan=2, pady=10)
-result_label.grid(row=2, column=0, columnspan=2, pady=10)
+    def fetch_price(self):
+        selected_crypto = self.crypto_combobox.get()
+        if selected_crypto:
+            price = self.get_crypto_prices(selected_crypto.lower())
+            if price is not None:
+                self.result_label.config(text=f"Current {selected_crypto.capitalize()} price: ${price}")
+            else:
+                self.result_label.config(text="Failed to fetch cryptocurrency price.")
 
-# Start the Tkinter event loop
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CryptoPriceCheckerApp(root)
+    root.mainloop()
